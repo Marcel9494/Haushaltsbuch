21470a5369e03b30409f47c86a01c02fc2c8c5c5
Marcixy <93829086+Marcel9494@users.noreply.github.com>
2023-04-01 17:47:24 +0200

    Monatliche Buchungsliste angefangen
    - Monatliche Buchungsliste angefangen

---
+++ lib/components/cards/monthly_overview_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MonthlyOverviewCard extends StatelessWidget {
+  const MonthlyOverviewCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: Text('Test'),
+    );
+  }
+}
--- lib/components/tab_views/monthly_tab_view.dart
+++ lib/components/tab_views/monthly_tab_view.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '/components/deco/loading_indicator.dart';
+import '/components/cards/monthly_overview_card.dart';
+
+import '../../models/booking.dart';
+
+import '../deco/overview_tile.dart';
+
 class MonthlyTabView extends StatefulWidget {
   const MonthlyTabView({Key? key}) : super(key: key);
 
@@ -8,10 +15,57 @@ class MonthlyTabView extends StatefulWidget {
 }
 
 class _MonthlyTabViewState extends State<MonthlyTabView> {
+  final List<Booking> _bookingList = [];
+
+  Future<List<Booking>> _loadYearlyBookingList() async {
+    return _bookingList;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Monatlich'),
+    return Expanded(
+      child: FutureBuilder(
+        future: _loadYearlyBookingList(),
+        builder: (BuildContext context, AsyncSnapshot<List<Booking>> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const LoadingIndicator();
+            case ConnectionState.done:
+              if (_bookingList.isEmpty) {
+                return Column(
+                  children: const [
+                    OverviewTile(
+                      shouldText: 'Einnahmen',
+                      should: 0,
+                      haveText: 'Ausgaben',
+                      have: 0,
+                      balanceText: 'Saldo',
+                      showAverageValuesPerDay: true,
+                      investmentText: 'Investitionen',
+                      showInvestments: true,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text('Noch keine Buchungen vorhanden.'),
+                      ),
+                    ),
+                  ],
+                );
+              } else {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: _bookingList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return const MonthlyOverviewCard();
+                    },
+                  ),
+                );
+              }
+            default:
+              return const Text('Warten');
+          }
+        },
+      ),
     );
   }
 }
