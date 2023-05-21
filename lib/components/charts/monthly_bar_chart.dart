01a8b49840327f4aeae645a1be29ba439d081540
Marcixy <93829086+Marcel9494@users.noreply.github.com>
2023-05-21 21:04:58 +0200

    Monatliche Statistik weiter verbessert
    - Monatliche Ausgaben Statistik Anzeige weiter verbessert. Der Benutzer kann nun durch die Monate navigieren (Vorgänger und Nachfolger).

--- lib/components/charts/monthly_bar_chart.dart
+++ lib/components/charts/monthly_bar_chart.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
+import '../buttons/month_picker_buttons.dart';
 import '/models/booking.dart';
 
 import '/utils/date_formatters/date_formatter.dart';
@@ -28,6 +29,7 @@ class MonthlyBarChartState extends State<MonthlyBarChart> {
   List<BarChartGroupData> _monthlyExpendituresBarGroups = [];
   List<BarChartGroupData> _reversedMonthlyExpendituresBarGroups = [];
   List<BarChartGroupData> _showingMonthlyExpendituresBarGroups = [];
+  DateTime _selectedDate = DateTime.now();
   final double width = 8;
   int touchedGroupIndex = -1;
 
@@ -41,7 +43,7 @@ class MonthlyBarChartState extends State<MonthlyBarChart> {
     _monthlyExpenditures = [];
     _monthlyExpendituresBarGroups = [];
     for (int i = 0; i < 7; i++) {
-      _bookingList = await Booking.loadMonthlyBookingList(DateTime.now().month - i, DateTime.now().year, widget.bookingList[0].categorie);
+      _bookingList = await Booking.loadMonthlyBookingList(_selectedDate.month - i, _selectedDate.year, widget.bookingList[0].categorie);
       if (_bookingList.isEmpty) {
         _monthlyExpenditures.insert(i, 0.0);
         _monthlyExpendituresBarGroups.add(makeGroupData(i, _monthlyExpenditures[i]));
@@ -67,19 +69,25 @@ class MonthlyBarChartState extends State<MonthlyBarChart> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Row(
-              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Expanded(
-                  child: Text(
-                    widget.bookingList.isEmpty ? '' : 'Monatliche Buchungen für ' + widget.bookingList[0].categorie,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(color: Colors.white, fontSize: 16.0),
+                  flex: 2,
+                  child: MonthPickerButtons(
+                    selectedDate: _selectedDate,
+                    selectedDateCallback: (DateTime selectedDate) {
+                      _selectedDate = selectedDate;
+                      _loadMonthlyBarChartData();
+                    },
                   ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: _monthlyExpenditures.isEmpty ? const Text('Gesamtsumme: 0,0 €') : Text('Gesamtsumme: ' + formatToMoneyAmount(_monthlyExpenditures[0].toString())),
                 ),
               ],
             ),
             const SizedBox(
-              height: 24.0,
+              height: 26.0,
             ),
             Expanded(
               child: BarChart(
@@ -145,14 +153,14 @@ class MonthlyBarChartState extends State<MonthlyBarChart> {
                       sideTitles: SideTitles(
                         showTitles: true,
                         getTitlesWidget: bottomMonthTitles,
-                        reservedSize: 32,
+                        reservedSize: 32.0,
                       ),
                     ),
                     leftTitles: AxisTitles(
                       sideTitles: SideTitles(
                         showTitles: true,
-                        reservedSize: 50,
-                        interval: 1,
+                        reservedSize: 50.0,
+                        interval: 1.0,
                         getTitlesWidget: leftTitles,
                       ),
                     ),
@@ -161,7 +169,21 @@ class MonthlyBarChartState extends State<MonthlyBarChart> {
                     show: false,
                   ),
                   barGroups: _showingMonthlyExpendituresBarGroups,
-                  gridData: FlGridData(show: false),
+                  gridData: FlGridData(
+                    show: true,
+                    getDrawingHorizontalLine: (value) {
+                      return FlLine(
+                        color: Colors.cyanAccent.withOpacity(0.12),
+                        strokeWidth: 0.8,
+                      );
+                    },
+                    getDrawingVerticalLine: (value) {
+                      return FlLine(
+                        color: Colors.cyanAccent.withOpacity(0.12),
+                        strokeWidth: 0.8,
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
@@ -196,13 +218,13 @@ class MonthlyBarChartState extends State<MonthlyBarChart> {
 
   Widget bottomMonthTitles(double value, TitleMeta meta) {
     final months = <String>[
-      dateFormatterMMM.format(DateTime.now()),
-      dateFormatterMMM.format(DateTime(DateTime.now().year, DateTime.now().month - 1)),
-      dateFormatterMMM.format(DateTime(DateTime.now().year, DateTime.now().month - 2)),
-      dateFormatterMMM.format(DateTime(DateTime.now().year, DateTime.now().month - 3)),
-      dateFormatterMMM.format(DateTime(DateTime.now().year, DateTime.now().month - 4)),
-      dateFormatterMMM.format(DateTime(DateTime.now().year, DateTime.now().month - 5)),
-      dateFormatterMMM.format(DateTime(DateTime.now().year, DateTime.now().month - 6)),
+      dateFormatterMMM.format(_selectedDate),
+      dateFormatterMMM.format(DateTime(_selectedDate.year, _selectedDate.month - 1)),
+      dateFormatterMMM.format(DateTime(_selectedDate.year, _selectedDate.month - 2)),
+      dateFormatterMMM.format(DateTime(_selectedDate.year, _selectedDate.month - 3)),
+      dateFormatterMMM.format(DateTime(_selectedDate.year, _selectedDate.month - 4)),
+      dateFormatterMMM.format(DateTime(_selectedDate.year, _selectedDate.month - 5)),
+      dateFormatterMMM.format(DateTime(_selectedDate.year, _selectedDate.month - 6)),
     ];
 
     final Widget text = Text(
