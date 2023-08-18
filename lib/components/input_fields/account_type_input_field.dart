57241a2e1f08dc5c721095bb6d4742659afa9c4a
Marcixy <93829086+Marcel9494@users.noreply.github.com>
2023-08-18 23:19:13 +0200

    Konto Typ Auswahl verbessert
    - Konto Typ Auswahl wurde verbessert wird nun in einem Bottom Sheet angezeigt und kann dort auch ausgewählt werden.
    - Aktuell selektierter Konto Typ wird ebenfalls markiert.
    - Nicht mehr benötigter List Dialog Code wurde entfernt.
    - Code formatiert

--- lib/components/input_fields/account_type_input_field.dart
+++ lib/components/input_fields/account_type_input_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
-import '../dialogs/list_dialog.dart';
+import '../deco/bottom_sheet_line.dart';
 
 import '/models/enums/account_types.dart';
 
@@ -24,7 +25,54 @@ class AccountTypeInputField extends StatelessWidget {
   }) : super(key: key);
 
   void _openBottomSheetWithAccountTypeList(BuildContext context) {
-    showListDialog(context, 'Konto Typen:', accountTypes, textController);
+    showCupertinoModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Material(
+          child: SizedBox(
+            height: 460.0,
+            child: ListView(
+              children: [
+                const BottomSheetLine(),
+                const Padding(
+                  padding: EdgeInsets.only(top: 16.0, left: 30.0),
+                  child: Text('Konto Typ auswählen:', style: TextStyle(fontSize: 18.0)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: SizedBox(
+                    height: 460.0,
+                    child: ListView.builder(
+                      itemCount: accountTypes.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 4.0),
+                          child: ListTile(
+                            title: Text(accountTypes[index], textAlign: TextAlign.center),
+                            onTap: () => {
+                              textController.text = accountTypes[index],
+                              Navigator.pop(context),
+                            },
+                            visualDensity: const VisualDensity(vertical: -4.0),
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                  color: textController.text == accountTypes[index] ? Colors.cyanAccent.shade400 : Colors.grey,
+                                  width: textController.text == accountTypes[index] ? 1.2 : 0.4),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            tileColor: Colors.grey.shade800,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
